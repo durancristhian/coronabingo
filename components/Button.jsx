257ceade98b17bd03814d431328cd3b0f7c61b11
Cristhian Duran <durancristhian@gmail.com>
2020-03-19 16:46:40 -0300

    fix styles

--- components/Button.jsx
+++ components/Button.jsx
@@ -12,11 +12,12 @@ export default function Button({
     <button
       type={type}
       className={classnames([
-        `bg-${color}-400 border-2 border-${color}-600 font-medium h-12 px-4 py-2 rounded text-center text-${color}-600 uppercase`,
-        `focus:bg-${color}-500 focus:border-${color}-700 focus:outline-none focus:shadow-outline focus:text-${color}-700`,
+        `border-2 font-medium h-12 px-4 py-2 rounded text-center uppercase`,
+        `focus:outline-none focus:shadow-outline`,
         'duration-150 ease-in-out transition',
         'disabled:opacity-50',
-        className
+        className,
+        [...COLORS[color]]
       ])}
       disabled={disabled}
       {...rest}
@@ -26,4 +27,39 @@ export default function Button({
       </span>
     </button>
   )
+}
+
+const COLORS = {
+  gray: [
+    `bg-gray-400`,
+    `border-gray-600`,
+    `text-gray-600`,
+    `focus:bg-gray-500`,
+    `focus:border-gray-700`,
+    `focus:text-gray-700`
+  ],
+  green: [
+    `bg-green-400`,
+    `border-green-600`,
+    `text-green-600`,
+    `focus:bg-green-500`,
+    `focus:border-green-700`,
+    `focus:text-green-700`
+  ],
+  red: [
+    `bg-red-400`,
+    `border-red-600`,
+    `text-red-600`,
+    `focus:bg-red-500`,
+    `focus:border-red-700`,
+    `focus:text-red-700`
+  ],
+  yellow: [
+    `bg-yellow-400`,
+    `border-yellow-600`,
+    `text-yellow-600`,
+    `focus:bg-yellow-500`,
+    `focus:border-yellow-700`,
+    `focus:text-yellow-700`
+  ]
 }
