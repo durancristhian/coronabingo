1b34900065c17b065cdb66de10ee33644d779d28
Cristhian Duran <durancristhian@gmail.com>
2020-03-19 14:23:12 -0300

    version

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
+import pkg from '../package.json'
+
 export default function Header() {
   return (
     <div className="bg-white p-4 shadow">
       <div className="max-w-5xl mx-auto">
         <div className="flex items-center justify-center">
           <span className="text-3xl">👑</span>
-          <h1 className="font-medium ml-2 mt-1 text-2xl">Bingo</h1>
+          <h1 className="font-medium ml-2 mr-4 mt-1 text-2xl">Bingo</h1>
+          <span className="mt-2 text-gray-500 text-sm">v{pkg.version}</span>
         </div>
       </div>
     </div>
