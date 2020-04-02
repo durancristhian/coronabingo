7e91acda8519975fa947f83b78c3c8431f5da944
Mateo Silguero <mfsilguero@gmail.com>
2020-04-02 09:18:53 -0300

    persist selected background cell on localStorage

--- contexts/BackgroundCellContext.tsx
+++ contexts/BackgroundCellContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
 import { BACKGROUND_CELL_VALUES } from '~/utils/constants'
 
 const defaultContextValue = {
@@ -17,9 +17,24 @@ interface IProps {
 const BackgroundCellContextProvider = ({ children }: IProps) => {
   let [backgroundCell, setBackgroundCell] = useState<ICell>(defaultContextValue)
 
+  useEffect(() => {
+    try {
+      setBackgroundCell(
+        JSON.parse(localStorage.getItem('backgroundCell') || '')
+      )
+    } catch (e) {}
+  }, [])
+
+  const saveAndSetBackgroundCell = (backgroundCell: ICell) => {
+    try {
+      setBackgroundCell(backgroundCell)
+      localStorage.setItem('backgroundCell', JSON.stringify(backgroundCell))
+    } catch (e) {}
+  }
+
   return (
     <BackgroundCellContext.Provider
-      value={{ backgroundCell, setBackgroundCell }}
+      value={{ backgroundCell, setBackgroundCell: saveAndSetBackgroundCell }}
     >
       {children}
     </BackgroundCellContext.Provider>
