5696bdfb4c5b92bdf57ab77fa6450e500d067122
Cristhian Duran <durancristhian@gmail.com>
2020-04-04 02:58:22 -0300

    More translations

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
   }
 
   return (
-    <div className="bg-white p-4 shadow">
+    <div className="bg-white px-4 py-2 shadow">
       <div className="max-w-4xl mx-auto">
         <div className="flex items-center justify-between">
           <h1 className="font-medium text-2xl">
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+// @ts-ignore
+import useTranslation from 'next-translate/useTranslation'
 import { ReactNode } from 'react'
 import Banner from './Banner'
 import Footer from './Footer'
@@ -8,19 +10,21 @@ interface IProps {
 }
 
 export default function Layout({ children }: IProps) {
+  const { t } = useTranslation()
+
   return (
     <main className="bg-gray-200 flex flex-col font-inter leading-none min-h-screen text-gray-900">
       <Header />
       <div className="flex-auto">{children}</div>
       <Banner>
-        <span>Dejanos tu feedback completando&nbsp;</span>
+        <span>{t('common:feedback-form-intro')} </span>
         <a
           href="https://forms.gle/egSBrsKSFnEgabff7"
           target="_blank"
           rel="noopener noreferrer"
           className="focus:outline-none focus:shadow-outline font-medium text-blue-600 underline"
         >
-          esta encuesta
+          {t('common:feedback-form-link')}
         </a>
         <span>&nbsp;🤩</span>
       </Banner>
