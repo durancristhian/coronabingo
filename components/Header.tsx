900a4378761d04a58aec3fab0f689716328b7b57
Cristhian Duran <durancristhian@gmail.com>
2020-04-04 02:35:14 -0300

    Add Select on header + first translation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,46 @@
 // @ts-ignore
+import Link from 'next-translate/Link'
+// @ts-ignore
+import Router from 'next-translate/Router'
+// @ts-ignore
 import useTranslation from 'next-translate/useTranslation'
+import { allLanguages } from '~/i18n.json'
+import Select from './Select'
 
 export default function Header() {
   const { t, lang } = useTranslation()
 
+  const languages = allLanguages.map(l => ({
+    id: l,
+    name: t(`common:language-${l}`)
+  }))
+
+  const onLanguageChange = (l: string) => {
+    Router.pushI18n({
+      url: '/',
+      options: {
+        lang: l
+      }
+    })
+  }
+
   return (
     <div className="bg-white p-4 shadow">
       <div className="max-w-4xl mx-auto">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-between">
           <h1 className="font-medium text-2xl">
-            <a href="/" className="focus:outline-none focus:shadow-outline">
-              Coronabingo
-            </a>
+            <Link href="/">
+              <a className="focus:outline-none focus:shadow-outline">
+                Coronabingo
+              </a>
+            </Link>
           </h1>
-          <p className="ml-8">
-            {lang} - {t('common:title')}
-          </p>
+          <Select
+            id="language"
+            onChange={onLanguageChange}
+            options={languages}
+            value={lang}
+          />
         </div>
       </div>
     </div>
