25ab5a7520ab29e06294b9fa1e5ec4290549693f
Cristhian Duran <durancristhian@gmail.com>
2020-04-04 00:52:22 -0300

    Add basic next translate implementation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,9 @@
+// @ts-ignore
+import useTranslation from 'next-translate/useTranslation'
+
 export default function Header() {
+  const { t, lang } = useTranslation()
+
   return (
     <div className="bg-white p-4 shadow">
       <div className="max-w-4xl mx-auto">
@@ -8,6 +13,9 @@ export default function Header() {
               Coronabingo
             </a>
           </h1>
+          <p>
+            {lang} - {t('common:title')}
+          </p>
         </div>
       </div>
     </div>
