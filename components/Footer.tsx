5ab839c8c3491051497b029ed6fb21d5091be274
Cristhian Duran <durancristhian@gmail.com>
2020-04-10 15:19:06 -0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
   }
 
   return (
-    <footer className="bg-white p-4 shadow">
+    <footer className="bg-white px-4 py-2 shadow">
       <div className="max-w-4xl mx-auto">
         <div className="flex items-center justify-between">
           <Button color="pink" onClick={donate}>
