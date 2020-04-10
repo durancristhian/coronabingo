a837821449f60ed43e56d2ef07daad2639772d8c
Cristhian Duran <durancristhian@gmail.com>
2020-04-10 13:24:09 -0300

    Improve scripts

---
+++ scripts/download-boards.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { writeFileSync } = require('fs')
+const { getWorksheet } = require('gsheets')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const fetch = require('isomorphic-unfetch')
+const { join } = require('path')
+
+if (!process.env.WOORKSHEET_ID || !process.env.WORKSHEET_TITLE) {
+  throw new Error(
+    [
+      '❌',
+      ' Configure "WOORKSHEET_ID" and/or "WORKSHEET_TITLE" as env var first',
+    ].join(''),
+  )
+}
+
+const downloadBoards = async () => {
+  try {
+    const worksheet = await getWorksheet(
+      process.env.WOORKSHEET_ID,
+      process.env.WORKSHEET_TITLE,
+    )
+    const boards = worksheet.data.map(Object.values)
+
+    writeFileSync(
+      join(__dirname, '../public', 'boards.json'),
+      JSON.stringify(boards, null, 2),
+    )
+
+    console.log('✅', ' Boards downloaded successfully')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+downloadBoards()
+
+export {}
---
+++ scripts/validate-locales.ts
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { readdirSync, readFileSync } = require('fs')
+const { join } = require('path')
+const { allLanguages, defaultLanguage } = require('../i18n.json')
+
+interface Locales {
+  [key: string]: {
+    [key: string]: string
+  }
+}
+
+const localesPath = join(__dirname, '../locales')
+// @ts-ignore
+const objectsHaveSameKeys = (...objects) => {
+  const allKeys = objects.reduce(
+    (keys, object) => keys.concat(Object.keys(object)),
+    [],
+  )
+  const union = new Set(allKeys)
+
+  return objects.every(object => union.size === Object.keys(object).length)
+}
+
+const allLocales = allLanguages.reduce((prev: Locales, curr: string) => {
+  const localePath = join(localesPath, curr)
+
+  console.log('🗂', ' Directory', localePath)
+
+  prev[curr] = {}
+
+  readdirSync(join(localesPath, curr)).forEach((file: string) => {
+    const filePath = join(localesPath, curr, file)
+
+    console.log('---- ', '📄', 'File', filePath)
+
+    prev[curr][file] = JSON.parse(readFileSync(filePath, 'utf-8'))
+  })
+
+  return prev
+}, {})
+
+const filesDefaultLanguage = allLocales[defaultLanguage]
+
+Object.keys(allLocales)
+  .filter(l => l !== defaultLanguage)
+  .map(l => {
+    const files = allLocales[l]
+    const comparisonOfFiles = objectsHaveSameKeys(filesDefaultLanguage, files)
+
+    console.log(comparisonOfFiles ? '✅' : '❌', defaultLanguage, l)
+
+    if (!comparisonOfFiles) {
+      throw new Error(
+        `Directories locales/${defaultLanguage} and locales/${l} don't have the same amount of files`,
+      )
+    }
+
+    Object.keys(files).map(k => {
+      const texts = files[k]
+      const comparisonOfTexts = objectsHaveSameKeys(
+        filesDefaultLanguage[k],
+        texts,
+      )
+
+      console.log(comparisonOfTexts ? '✅' : '❌', defaultLanguage, l, k)
+
+      if (!comparisonOfTexts) {
+        throw new Error(
+          `Between languages ${defaultLanguage} and ${l}, the keys of ${k} are not the same`,
+        )
+      }
+    })
+  })
+
+export {}
