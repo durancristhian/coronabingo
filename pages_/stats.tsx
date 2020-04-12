e3a57205d224ad240b879e40618aaab3d424461e
Cristhian Duran <durancristhian@gmail.com>
2020-04-12 03:32:03 -0300

    Remove code

--- pages_/stats.tsx
+++ pages_/stats.tsx
@@ -6,9 +6,6 @@ import Message from '~/components/Message'
 import db from '~/utils/firebase'
 
 interface Stats {
-  playersByRoom: {
-    [key: string]: number
-  }
   roomsByDay: {
     [key: string]: number
   }
@@ -20,24 +17,16 @@ interface Stats {
     date: string
     value: number
   }[]
-  topPlayersByRoom: {
-    roomId: string
-    players: number
-  }[]
-  totalPlayers: number
   totalRooms: number
 }
 
 const defaultStats = {
-  playersByRoom: {},
   roomsByDay: {},
   roomsConfigured: 0,
   roomsOfFamilies: 0,
   roomsWithOnlineBingoSpinner: 0,
   roomsWithVideoCall: 0,
   sortedRoomsByDay: [],
-  topPlayersByRoom: [],
-  totalPlayers: 0,
   totalRooms: 0,
 }
 const pad = (n: number) => n.toString().padStart(2, '0')
@@ -80,14 +69,6 @@ const transformRoomsByDay = (obj: Record<string, number>) =>
     }))
     .sort((r1, r2) => r2.value - r1.value)
     .slice(0, 10)
-const transformPlayersByRoom = (obj: Record<string, number>) =>
-  Object.keys(obj)
-    .map(k => ({
-      roomId: k,
-      players: obj[k],
-    }))
-    .sort((r1, r2) => r2.players - r1.players)
-    .slice(0, 10)
 const weekdays = [
   'Domingo',
   'Lunes',
@@ -104,21 +85,7 @@ export default function Index() {
   useEffect(() => {
     const getMarker = async () => {
       const roomsCollection = await db.collection('rooms').get()
-      const rooms = await Promise.all(
-        roomsCollection.docs.map(
-          doc =>
-            new Promise<firebase.firestore.DocumentData>(async resolve => {
-              const players = await db
-                .collection(`rooms/${doc.id}/players`)
-                .get()
-
-              resolve({
-                ...doc.data(),
-                players: players.docs.map(p => p.data()),
-              })
-            }),
-        ),
-      )
+      const rooms = roomsCollection.docs.map(r => r.data())
 
       console.log('// ROOMS')
       console.log(rooms)
@@ -128,7 +95,7 @@ export default function Index() {
         (prev: Stats, curr: firebase.firestore.DocumentData) => {
           prev.totalRooms++
 
-          if ('readyToPlay' in curr) {
+          if ('readyToPlay' in curr && curr.readyToPlay) {
             prev.roomsConfigured++
 
             const date = curr.date.toDate()
@@ -142,10 +109,6 @@ export default function Index() {
               weekdays[date.getDay()]
 
             prev.roomsByDay[dateKey] = (prev.roomsByDay[dateKey] || 0) + 1
-
-            prev.playersByRoom[curr.id || curr.name] = curr.players.length
-
-            prev.totalPlayers += curr.players.length
 
             if (
               curr.name.toUpperCase().includes('flia'.toUpperCase()) ||
@@ -169,7 +132,6 @@ export default function Index() {
       )
 
       data.sortedRoomsByDay = transformRoomsByDay(data.roomsByDay)
-      data.topPlayersByRoom = transformPlayersByRoom(data.playersByRoom)
 
       console.log('// DATA')
       console.log(data)
@@ -256,13 +218,6 @@ export default function Index() {
                     stats.roomsConfigured,
                   ) + '%',
                 )}
-                {renderRow(
-                  'Promedio de personas por sala configurada',
-                  percentage(stats.totalPlayers, stats.roomsConfigured),
-                  false,
-                  '',
-                  '',
-                )}
               </div>
               <div className="mt-8">
                 <h2 className="font-medium text-md md:text-lg">
@@ -279,18 +234,6 @@ export default function Index() {
                       date.split(',')[1],
                       '',
                     )}
-                  </Fragment>
-                ))}
-              </div>
-              <div className="mt-8">
-                <h2 className="font-medium text-md md:text-lg">
-                  Las 5 salas con más personas
-                </h2>
-              </div>
-              <div className="border-gray-300 border-l-2 border-r-2 border-t-2 flex flex-col mt-8 rounded">
-                {stats.topPlayersByRoom.map(({ roomId, players }, i) => (
-                  <Fragment key={roomId}>
-                    {renderRow(roomId, players, i % 2 !== 0, '', '')}
                   </Fragment>
                 ))}
               </div>
