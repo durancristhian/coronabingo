aa261269e3081296b028317a00624df96a8eea80
Cristhian Duran <durancristhian@gmail.com>
2020-04-14 15:11:28 -0300

    Add staging message

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import React, { ReactNode } from 'react'
+import { version } from '~/package.json'
 import Anchor from './Anchor'
 import Banner from './Banner'
 import Footer from './Footer'
@@ -14,14 +15,19 @@ export default function Layout({ children }: Props) {
   const { t } = useTranslation()
   const router = useRouter()
   const playerId = router.query.playerId?.toString()
+  const isStaging =
+    typeof window !== 'undefined'
+      ? window.location.hostname.split('.').includes('cduran')
+      : false
 
   return (
     <main className="bg-gray-200 flex flex-col font-sans leading-normal min-h-screen text-gray-900 text-sm md:text-base">
+      {isStaging && (
+        <Banner type="emphasis">{t('common:staging', { version })}</Banner>
+      )}
       <Header />
       {playerId && (
-        <Banner type="emphasis">
-          <span>{t('common:post-your-photo')} </span>
-        </Banner>
+        <Banner type="emphasis">{t('common:post-your-photo')}</Banner>
       )}
       <div className="flex-auto">
         <div className="px-4 py-8">{children}</div>
