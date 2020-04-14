49dc12dfd34953ad8fc42b280e42d06cc1860c13
Mateo Silguero <mfsilguero@gmail.com>
2020-04-14 16:42:40 -0300

    promise finally polyfill (ios < 11.0)

---
+++ utils/polyfills.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+
+// Promise finally()
+if (typeof Promise.prototype.finally !== 'function') {
+  // TIL: on safari ios 11, finally is not undefined, is and object
+  // so we must to deleted to implement the polyfill
+  // i mean, WTF ?!
+  delete Promise.prototype.finally
+  require('promise-polyfill/dist/polyfill.min.js')
+}
