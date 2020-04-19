80097148de05c900ea8c60f586a145625d92864e
Cristhian Duran <durancristhian@gmail.com>
2020-04-19 16:17:50 -0300

    Add link to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Router from 'next-translate/Router'
 import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
 import React from 'react'
 import Heading from '~/components/Heading'
 import { allLanguages } from '~/i18n.json'
@@ -7,6 +8,7 @@ import Select from './Select'
 
 export default function Header() {
   const { t, lang } = useTranslation()
+  const router = useRouter()
 
   const languages = allLanguages.map(l => ({
     id: l,
@@ -14,14 +16,13 @@ export default function Header() {
   }))
 
   const onLanguageChange = (l: string) => {
-    const { asPath, replaceI18n } = Router
     const slash = '/'
-    const url = asPath
+    const url = router.asPath
       .split(slash)
       .filter(p => p && !allLanguages.includes(p))
       .join(slash)
 
-    return replaceI18n({
+    return Router.replaceI18n({
       url: url || slash,
       options: {
         lang: l,
@@ -29,11 +30,19 @@ export default function Header() {
     })
   }
 
+  /*
+    By default, next-translate adds the default language (es) to the URL
+    and we want to avoid that only in the home page
+  */
+  const href = router.asPath === '/' ? '/' : `/${lang}`
+
   return (
     <header className="bg-white px-4 py-2 shadow">
       <div className="max-w-4xl mx-auto">
         <div className="flex items-center justify-between">
-          <Heading type="h1">Coronabingo</Heading>
+          <Heading type="h1">
+            <a href={href}>Coronabingo</a>
+          </Heading>
           <Select
             id="language"
             onChange={onLanguageChange}
