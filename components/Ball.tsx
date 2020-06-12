f1230429425a6a65b22f379ed60e9a315eb0795b
Cristhian Duran <durancristhian@gmail.com>
2020-06-12 12:32:44 -0300

    useMemo

--- components/Ball.tsx
+++ components/Ball.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames'
-import React, { Fragment } from 'react'
+import React, { Fragment, useMemo } from 'react'
 import Pelotita from './Pelotita'
 
 interface Props {
@@ -17,27 +17,30 @@ export default function Ball({
 }: Props) {
   const Wrapper = animate ? Pelotita : Fragment
 
-  return (
-    <Wrapper>
-      <div className={classnames(['text-center'])}>
-        <div
-          className={classnames(['ball', bgColor])}
-          style={{
-            height: `${size}px`,
-            width: `${size}px`,
-          }}
-        >
-          <div>
-            <span
-              data-test-class="ball"
-              className="font-medium"
-              style={{ fontSize: `${size / 3}px` }}
-            >
-              {number}
-            </span>
+  return useMemo(
+    () => (
+      <Wrapper>
+        <div className={classnames(['text-center'])}>
+          <div
+            className={classnames(['ball', bgColor])}
+            style={{
+              height: `${size}px`,
+              width: `${size}px`,
+            }}
+          >
+            <div>
+              <span
+                data-test-class="ball"
+                className="font-medium"
+                style={{ fontSize: `${size / 3}px` }}
+              >
+                {number}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    ),
+    [number],
   )
 }
