99384dd6989d235f77b62bdc67d7e9b02e5dbdb5
Cristhian Duran <durancristhian@gmail.com>
2020-06-22 02:41:15 -0300

    Improve container

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,15 +7,15 @@ interface Props {
 }
 
 const CONTAINER_SIZES = {
-  small: 'md:w-2/5',
-  medium: 'md:w-3/5',
-  large: 'w-full',
+  small: ['md:w-6/12', 'lg:w-2/5'],
+  medium: ['md:w-8/12', 'lg:w-3/5'],
+  large: ['w-full'],
 }
 
 export default function Container({ children, size = 'small' }: Props) {
   return (
     <div className="max-w-6xl mx-auto">
-      <div className={classnames(['mx-auto', CONTAINER_SIZES[size]])}>
+      <div className={classnames(['mx-auto', [...CONTAINER_SIZES[size]]])}>
         {children}
       </div>
     </div>
